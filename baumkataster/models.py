d91614b7cc022ef00047d7177572950e8ea826d0
LaurensWink <laurensherzebrock@googlemail.com>
2022-10-26 20:41:04 +0200

    user model

--- baumkataster/models.py
+++ baumkataster/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
