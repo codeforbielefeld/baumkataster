4098d85a62a0de844540609300f9fdc3a03d1116
LaurensWink <laurensherzebrock@googlemail.com>
2022-10-26 20:49:11 +0200

    Tree model

--- baumkataster/models.py
+++ baumkataster/models.py
@@ -5,3 +5,12 @@ class User(models.Model):
     name = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
+
+class Tree(models.Model):
+    name = models.CharField(max_length=100)
+    height = models.IntegerField(default=0)
+    diameter = models.IntegerField(default=0)
+    kind = models.CharField(max_length=100)
+    long = models.DecimalField(max_digits=9, decimal_places=6)
+    lat = models.DecimalField(max_digits=9, decimal_places=6)
+    type_of_care = models.IntegerField(default=0)
