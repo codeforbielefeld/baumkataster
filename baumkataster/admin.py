ba1b9d9461a6cfe18bf6ca5f2f9878144bbd57b8
LaurensWink <laurensherzebrock@googlemail.com>
2022-10-26 21:47:39 +0200

    register Tree and User

--- baumkataster/admin.py
+++ baumkataster/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+# Register your models here
+
+from .models import Tree
+from .models import User
+
+admin.site.register(Tree)
+admin.site.register(User)
