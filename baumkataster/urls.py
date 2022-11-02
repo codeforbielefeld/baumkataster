222683864f6124b4539a8c5d8f65f71aa0661191
Helen Bielawa <helen.bielawa@web.de>
2022-11-02 22:01:54 +0100

    add_tree definiert

--- baumkataster/urls.py
+++ baumkataster/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from baumkataster.views import hello_world
+from baumkataster.views import add_tree, hello_world
 
 
 urlpatterns = [
     path('hello', hello_world),
+    path('add_tree', add_tree)
 ]
